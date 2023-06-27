ea39cb7a60eb766b9dbe45d2832be7bc6234ff27
daudi <auditory@magezi.com>
2023-06-27 19:25:03 +0200

    updated folder selection

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -7,8 +7,11 @@
 int main(int argc, char **argv){
     QApplication app(argc,argv);
     QString folder; 
-    folder = QFileDialog::getOpenFileName();
-
+    folder = QFileDialog::getExistingDirectory(nullptr,
+            QFileDialog::tr("Select directory"),
+            "/home",
+            QFileDialog::ShowDirsOnly 
+            | QFileDialog::DontResolveSymlinks);
     GUI::Image image(folder);
     image.show();
     
